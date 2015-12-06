3cd13752cf348302181c127447170199620d4247
BbDgtl <bbdgtl@gmail.com>
2015-12-06 15:37:07 -0800

    create framework in HTML & begin Javascript

---
+++ shelter.js
@@ -0,0 +1,9 @@
+    // var danger = prompt("Are you in danger?").toLowerCase();
+    // if ((danger === "yes") || (danger === "y")) {
+    //   alert("please contact 911");
+    // }
+
+function form() {
+  var el = document.getElementById("onSubmit");
+  el.onClick =
+}
