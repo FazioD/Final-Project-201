d0b82e3f705e6951fb7dc9da604e31549f3045a8
BbDgtl <bbdgtl@gmail.com>
2015-12-08 19:26:55 -0800

    create framework in HTML & begin Javascript

--- shelter.js
+++ shelter.js
@@ -1,9 +1,15 @@
-    // var danger = prompt("Are you in danger?").toLowerCase();
-    // if ((danger === "yes") || (danger === "y")) {
-    //   alert("please contact 911");
-    // }
+//Radio Button for yes danger.
+//parent/child relationship to target each specific function.
+$(document).ready(function() {
+  $("#yesDanger").click(function() {
+    alert("Call 911");
+  });
+  $("#noDanger").click(function() {
+    alert("Don't Call 911");
+  });
+});
 
-function form() {
-  var el = document.getElementById("onSubmit");
-  el.onClick =
-}
+//INPUTS INTO POSSIBLE BACK END DATABASES
+
+//RADIO BUTTONS for Do you have children.
+
