9aa2d9b7af0d676c61d95252dca7a6160ad19f07
BbDgtl <bbdgtl@gmail.com>
2015-12-08 20:29:58 -0800

    create framework in HTML & begin Javascript

--- shelter.js
+++ shelter.js
@@ -1,3 +1,9 @@
+//Global Scope Variables.
+var clientName = $("#nameInput");
+var zipcode = $("zipCode");
+var numberOfKids = Number($("numberKids"));
+var info = $("textArea");
+
 //Radio Button for yes danger.
 //parent/child relationship to target each specific function.
 $(document).ready(function() {
@@ -12,4 +18,33 @@ $(document).ready(function() {
 //INPUTS INTO POSSIBLE BACK END DATABASES
 
 //RADIO BUTTONS for Do you have children.
+$(document).ready(function() {
+  $("#yesKids").click(function() {
+    alert("Call 911");
+  });
+  $("#noKids").click(function() {
+    alert("Don't Call 911");
+  });
+});
 
+//INPUT for HOW MANY CHILDREN.
+
+//RADIO BUTTONS for the "Are you in need of medical assistance."
+$(document).ready(function() {
+  $("#yesCare").click(function() {
+    alert("Call 911");
+  });
+  $("#noCare").click(function() {
+    alert("Don't Call 911");
+  });
+});
+
+//RADIO BUTTONS for the ? "Would you like to speak with one of our..."
+$(document).ready(function() {
+  $("#yesAbuse").click(function() {
+    alert("Call 911");
+  });
+  $("#noAbuse").click(function() {
+    alert("Don't Call 911");
+  });
+});
