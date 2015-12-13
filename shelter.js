35dc09dd1025e57be289ccf29a5159cd939af3d5
FazioD <faziod@uw.edu>
2015-12-13 13:53:25 -0800

    resolving conflicts

--- shelter.js
+++ shelter.js
@@ -57,9 +57,19 @@ $(function() {
   $("#kids [type=radio]").click(function() {
     if (children) {
       $("#hiddenKids").css("display", "inline");
+    } else {
+      $("#hiddenKids").css("display", "none");
     }
   });
 });
+//IF USER CLICKS NO, HIDE SECTION
+// $(function() {
+//   $("#kids [type=radio]").click(function() {
+//     if (children === false) {
+//       $("#hiddenKids").css("display", "none");
+//     }
+//   });
+// });
 
 //RADIO BUTTONS for the "Are you in need of medical assistance."
 $(function() {
