47cf2683747d36a29737e5ae6cfd08a7dc2cdf1a
BbDgtl <bbdgtl@gmail.com>
2015-12-15 17:18:12 -0800

    add functionality to children section

--- shelter.js
+++ shelter.js
@@ -1,23 +1,24 @@
 //Global Scope Variables.
-//DATE.
+//DATE
 var savedDate = $("#date");
-//IN DANGER.
+//IN DANGER
 var inDanger = false;
-//NAME SECTION.
+//NAME SECTION
 var firstName = $("#firstName");
 var lastName = $("#lastName");
 var middleName = $("#middle");
 var maidenName = $("#maiden");
-//ADDRESS SECTION.
+//ADDRESS SECTION
 var address1 = $("#address1");
 var address2 = $("#address2");
 var city = $("#city");
 var state = $("#state");
 var address = Number($("#zipCode"));
-//CHILDREN SECTION.
+//CHILDREN SECTION
 var children = false;
 var childsFirst = $(".firstNames");
 var childsSecond = $(".lastNames");
+var childArray = [];
 //MEDICAL CARE
 var medicalCare = false;
 //SUBSTANCE ABUSE SECTION
@@ -33,14 +34,16 @@ $(function() {
     $("#alert").html("Call 911");
   });
   $("#noDanger").click(function() {
-    console.log("Client is not in immediate danger.");
     inDanger = false;
+    console.log("Client is not in immediate danger.");
+    $("#alert").html("");
   });
 });
 
 //Inputs for Full Name Section in HTML is at this location.
 //Input for Last Known Address in HTML is at this location.
 
+// *************** CHILDREN SECTION: BOBBY **************** //
 //RADIO BUTTONS for children section.
 $(function() {
   $("#yesKids").click(function() {
@@ -52,23 +55,46 @@ $(function() {
     children = false;
   });
 });
+
 //INCLUDE ADDING NEW CHILDREN FUNCTION HERE.
 $(function() {
   $("#kids [type=radio]").click(function() {
     if (children) {
       $("#hiddenKids").css("display", "inline");
-    }
-  });
-});
-$(function() {
-  $("#kids [type=radio]").click(function() {
-    if (children === false) {
+    } else {
       $("#hiddenKids").css("display", "none");
     }
   });
 });
 
-//RADIO BUTTONS for the "Are you in need of medical assistance."
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".input_fields_wrap"); //Fields wrapper
+    var add_button = $(".add_field_button"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append("<div><label>First: </label><input type='text' name='mytext[]'><label>Last: </label><input type='text' name='mytext[]'></div></div>"); //add input box
+        }
+    });
+
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    });
+});
+//Adding event listener to submit children to childArray.
+$(function() {
+  $("#addChild").click(function() {
+    childArray.push($(".childName"));
+    console.log(childArray);
+  });
+});
+// ***************END CHILDREN SECTION***************** //
+
+//RADIO BUTTONS for "Are you in need of medical assistance."
 $(function() {
   $("#yesCare").click(function() {
     console.log("Client needs medical care.");
@@ -80,7 +106,7 @@ $(function() {
   });
 });
 
-//RADIO BUTTONS for the ? "Would you like to speak with one of our..."
+//RADIO BUTTONS for the "Would you like to speak with one of our..."
 $(function() {
   $("#yesAbuse").click(function() {
     console.log("Client is seeking assistance with substance abuse.");
@@ -95,11 +121,3 @@ $(function() {
 //Submit Button Event Listener
 var newSubmitButton = document.getElementById("add-new-submit");
 newShopButton.addEventListener("click", handelSubmit);
-
-//$(function() {
-//this replaces document.ready
-
-//write a function that takes argument like elements collecting info
-//second argument what is done with the info, or specify info
-//call function with jquery variables as parameters for arguments
-
